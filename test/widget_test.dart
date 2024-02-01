d6324f4ec693fa651e3f8ba287ea763264141627
obutora <obutora@gmail.com>
2024-02-01 20:35:24 +0900

    Refactor homeEventsTest variable to homeEvents

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,29 +7,31 @@ import 'package:riverpod_mix_in/todo/todo.dart';
 
 void main() {
   final homeStates = HomeStateTest();
-  final homeEventsTest = HomeEventsTest();
+  final homeEvents = HomeEventsTest();
 
+  // 例1
   test('Add todo', () {
     final container = ProviderContainer();
-    homeEventsTest.addTodo(container, 'test');
+    homeEvents.addTodo(container, 'test');
     expect(container.read(todoListProvider).length, 1);
     expect(container.read(todoListProvider).first.title, 'test');
 
-    homeEventsTest.addTodo(container, 'test2');
+    homeEvents.addTodo(container, 'test2');
     expect(container.read(todoListProvider).length, 2);
 
-    homeEventsTest.removeTodoAt(container, 0);
+    homeEvents.removeTodoAt(container, 0);
     expect(container.read(todoListProvider).length, 1);
     expect(container.read(todoListProvider).first.title, 'test2');
   });
 
+  //例2
   test('search todo', () async {
     final container = ProviderContainer(
         // 必要に応じてProviderのoverrideを行うことも可能
         );
 
     // 初期Todoを追加
-    homeEventsTest.addTodo(container, 'test');
+    homeEvents.addTodo(container, 'test');
     expect(container.read(todoListProvider).length, 1);
     expect(container.read(todoListProvider).first.title, 'test');
 
@@ -38,7 +40,7 @@ void main() {
     expect(homeStates.textController(container).text, 'test');
 
     // 検索実行
-    homeEventsTest.search(container);
+    homeEvents.search(container);
 
     // 初期ステートはLoading
     expect(
@@ -49,7 +51,7 @@ void main() {
     // 検索中の状態が適切に反映される
     expect(homeStates.isSearching(container), true);
 
-    // 検索結果が反映される
+    // 検索結果が正常に反映される
     await Future.delayed(const Duration(seconds: 1));
     expect(homeStates.searchedTodo(container).value, [
       isA<Todo>().having((e) => e.title, 'title', 'test'),
